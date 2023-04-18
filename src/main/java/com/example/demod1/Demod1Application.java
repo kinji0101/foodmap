c8698c9e2650a4116dfd6035d07c3046b9aa0b56
MSI\kinji <kidrrrooobbb@gmail.com>
2023-04-18 17:39:17 +0800

    1

---
+++ src/main/java/com/example/demod1/Demod1Application.java
@@ -0,0 +1,13 @@
+package com.example.demod1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demod1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demod1Application.class, args);
+	}
+
+}
